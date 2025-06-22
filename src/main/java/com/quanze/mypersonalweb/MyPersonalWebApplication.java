bdcb017eb68ec10e4a0f203cbeba67f2bfdf3579
wangq <wangquanze922@gmail.com>
2025-06-22 10:33:01 +0800

    commit

---
+++ src/main/java/com/quanze/mypersonalweb/MyPersonalWebApplication.java
@@ -0,0 +1,13 @@
+package com.quanze.mypersonalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPersonalWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quanze/mypersonalweb/MyPersonalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanze.mypersonalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPersonalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
