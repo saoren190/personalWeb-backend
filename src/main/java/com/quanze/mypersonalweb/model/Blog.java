cbbc019c545daa649ab3405fbe6226cd5e9bffda
wangq <wangquanze922@gmail.com>
2025-06-23 08:55:18 +0800

    Initial commit: backend setup with MySQL

---
+++ src/main/java/com/quanze/mypersonalweb/model/Blog.java
@@ -0,0 +1,4 @@
+package com.quanze.mypersonalweb.model;
+
+public class Blog {
+}
---
+++ src/main/resources/DB-init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE blogs (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       title VARCHAR(255) NOT NULL,
+                       content TEXT NOT NULL,
+                       created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                       updated_at DATETIME DEFAULT NULL
+);
